4b34dbea6a2e69bc452e40e4b16b57023fa4f3fb
Nick Bolton <nick@synergy-project.org>
2011-01-02 16:55:20 +0000

    added hack for listing not found

--- Rensoft.ManxAds.WebSite/ListingDetailsAdmin.ascx.cs
+++ Rensoft.ManxAds.WebSite/ListingDetailsAdmin.ascx.cs
@@ -4,20 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ManxAds;
 
 namespace Rensoft.ManxAds.WebSite
 {
     public partial class ListingDetailsAdmin : StandardControl
     {
-        protected override void Page_Load(object sender, EventArgs e)
+        public void Initialize(Listing listing)
         {
-            // HACK: this can be called from the index page when there is no listing,
-            // so just drop out here if no listing was requested.
-            if (!Accessors.HasListing)
-                return;
-
-            EditListingHyperLink.NavigateUrl = Accessors.Listing.ModifyUrl;
-            EditUserHyperLink.NavigateUrl = Accessors.Listing.Seller.ModifyUrl;
+            EditListingHyperLink.NavigateUrl = listing.ModifyUrl;
+            EditUserHyperLink.NavigateUrl = listing.Seller.ModifyUrl;
         }
     }
 }
