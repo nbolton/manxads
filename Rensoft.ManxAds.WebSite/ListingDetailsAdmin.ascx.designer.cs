3f65ffa4437a9e154c1ae62030fc569e9b74c9b7
Nick Bolton <nick@synergy-project.org>
2011-03-10 14:01:57 +0000

    Feature #313 - last known ip now shown on listings details, added get satisfaction link

--- Rensoft.ManxAds.WebSite/ListingDetailsAdmin.ascx.designer.cs
+++ Rensoft.ManxAds.WebSite/ListingDetailsAdmin.ascx.designer.cs
@@ -48,5 +48,14 @@ namespace Rensoft.ManxAds.WebSite {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink BanUserHyperLink;
+        
+        /// <summary>
+        /// LastKnownIpLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LastKnownIpLabel;
     }
 }
